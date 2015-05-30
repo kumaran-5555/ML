796ada0747e391e256104693dc3c02947a57f78c
kumaran_5555 <kumaran_5555@outlook.com>
2015-05-30 19:54:21 +0530

    Split commit

--- ML/ML.py
+++ ML/ML.py
@@ -1,9 +1,12 @@
 
 import numpy
+import random
+
+
 
 # test commenting
 class GiniImpurity:    
-    def __init__(self, start, end, n_classes, label_y, sample_weight_y, samples):
+    def __init__(self, start, end, n_classes, label_y, sample_weight_y, samples, total_training_weight):
         self.start = start
         self.end = end
         self.n_classes = n_classes
@@ -14,6 +17,7 @@ class GiniImpurity:
         self.curr_pos = start
         self.sample_weight_y = sample_weight_y
         self.label_y = label_y
+        self.total_training_weight = total_training_weight
         self.total_weight = 0
         self.total_left_weight = 0
         self.total_right_weight = 0
@@ -62,9 +66,127 @@ class GiniImpurity:
             temp = self.label_count_right[i]
             right += temp * temp
 
-        return 1.0 - left / (self.total_left_weight * self.total_left_weight), \
+        v1,v2 = 1.0 - left / (self.total_left_weight * self.total_left_weight), \
             1.0 - right / (self.total_right_weight * self.total_right_weight)
-    
+        #print(self.curr_pos, v1, v2)
+        return v1,v2
+
+    def improvement(self):
+        '''
+        Weighted impurity improvement, i.e.
+           N_t / N * (impurity - N_t_L / N_t * left impurity
+                               - N_t_L / N_t * right impurity),
+           where N is the total number of samples, N_t is the number of samples
+           in the current node, N_t_L is the number of samples in the left
+           child and N_t_R is the number of samples in the right child.
+        '''
+        left_impurity,right_impurity = self.child_impurity()
+        imp = (self.total_weight / self.total_training_weight * self.impurity()) -  \
+            ((self.total_left_weight / self.total_training_weight * left_impurity) + \
+            (self.total_right_weight / self.total_training_weight * right_impurity))
+        #print(self.curr_pos, imp)
+        return imp
+
+
+class SplitRecord:
+    def __init__(self):
+        self.start = None
+        self.end = None
+        self.value = None
+        self.impurity = None
+        self.left_impurity = None
+        self.right_impurity = None
+        self.improvement = None
+        self.feature_idx = None
+
+class Splitter:
+    def __init__(self, start, end, n_classes, label_y, data_x, sample_weight_y, samples, total_training_weight, seed,\
+        min_samples_leaf, min_weight_leaf):
+        '''
+        initialize for each node that has to be split, use split method to figure our best split for this node
+        returns SplitRecord
+        '''
+        self.start = start
+        self.end = end
+        self.n_classes = n_classes
+        self.label_y = label_y
+        self.data_x = data_x
+        self.sample_weight_y = sample_weight_y
+        self.samples = samples
+        self.total_training_weight = total_training_weight
+        self.n_features = data_x.shape[1]
+        self.seed = seed
+        self.n_samples = self.end-self.start
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_leaf = min_weight_leaf
+
+
+
+
+    def split(self):
+        '''
+        tries each features for split and return best one with split value
+        assumes node is splitable
+        '''
+
+        f_i = [i for i in range(self.n_features)]
+
+        split_rec = SplitRecord()
+        split_rec.start = self.start
+        split_rec.end = self.end
+        
+        '''
+        going to use all features, so no need to randomize the pick
+        # randomly pick a feature and check best split point
+        # choose the best among all features
+        random.seed(self.seed)
+        random.shuffle(f_i)
+        '''
+        best_improvement = float('-inf')
+        best_v = None
+        best_f = None
+        for f in f_i:
+            x_i = self.data_x[self.start:self.end, f]
+            sorted_samples = numpy.argsort(x_i) + self.start
+            gini = GiniImpurity(0, self.n_samples, self.n_classes, self.label_y, self.sample_weight_y, sorted_samples, self.total_training_weight)
+            
+            for i in range(1, self.n_samples):
+                gini.update(i)
+                if i < self.min_samples_leaf or self.n_samples < self.min_samples_leaf or \
+                    gini.total_left_weight < self.min_weight_leaf or gini.total_right_weight < self.min_weight_leaf:
+                    # bad split, continue
+                    continue
+
+
+                if best_improvement < gini.improvement():
+                    best_improvement = gini.improvement()
+                    best_v = (x_i[sorted_samples[i-1]] + x_i[sorted_samples[i]]) / 2
+                    best_f = f
+
+                    split_rec.feature_idx = f
+                    split_rec.impurity = gini.impurity()
+                    split_rec.left_impurity,split_rec.right_impurity = gini.child_impurity()
+                    split_rec.value = best_v
+                    
+        s = self.start
+        e = self.end
+        while s < e:             
+            if self.data_x[s, best_f] <= best_v:
+                # on the right side
+                s += 1
+                continue
+            else:
+                e -= 1
+                # have to move to right side
+                tmp = self.samples[e]
+                self.samples[e] = self.samples[s]
+                self.samples[s] = tmp
+
+        
+
+        return split_rec
+        #return best_improvement,best_v,best_f
+
 
 
 
@@ -76,30 +198,16 @@ sample_weight_y =  numpy.ones(dim_x)
 
 data = numpy.empty((dim_x,dim_y))
 label = numpy.empty(dim_x)
+samples = numpy.empty(dim_x)
 #next(f)
 
 for i,d in enumerate(f):
     data[i] = numpy.asarray(d.split(',')[:-1], dtype=numpy.float)
     label[i] = numpy.asarray(d.split(',')[-1], dtype=numpy.int)
+    samples[i] = i
 
-s = numpy.argsort(data[:,3])
-print(data[:,3])
-print(s)
-#print(sorted(s))
-
-gini = GiniImpurity(0,dim_x, 3, label, sample_weight_y, s)
-
-
-
-print(gini.impurity())
-
-for i in range(dim_x):
-    gini.update(i)
-    print(i, gini.child_impurity())
-
-
-
-
+s = Splitter(0, dim_x, 3, label, data, sample_weight_y, samples, dim_x, 1, 1, 1)
+print(s.split())
 
 
 
